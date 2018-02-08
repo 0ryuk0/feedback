db13e2c0e5eec858136c79779b817106fe6278c5
neha0993 <33566747+neha0993@users.noreply.github.com>
2018-02-08 16:49:08 +0530

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,4 @@
+IF EXIST C:\Worksapce\GCSPortal\Main\src\Support\Support.ClientApps_Restructured\node; SET PATH=%PATH%;C:\Worksapce\GCSPortal\Main\src\Support\Support.ClientApps_Restructured\node;
+IF EXIST C:\Worksapce\GCSPortal\Main\src\Support\Support.ClientApps_Restructured\node_modules\@angular\cli\bin; SET PATH=%PATH%;C:\Worksapce\GCSPortal\Main\src\Support\Support.ClientApps_Restructured\node_modules\@angular\cli\bin;
+ng build --base-href ./
+
